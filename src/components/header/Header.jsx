8fd38a7f6a25f3ad3ffa1866c24838cceefbabe7
Michel PC <0801342@csfoy.ca>
2022-03-15 13:06:55 -0400

    Creation of social tags and link with outside links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
+import ME from '../../assets/michelkl.jpeg';
+import HeaderSocials from './HeaderSocials';
+
 
 const Header = () => {
     return (
         <header>
+
             <div className="container header_container">
+                {/* Introduction */}
                 <h5>Hello I'm</h5>
                 <h1>Michel Kingma-Lord</h1>
                 <h5 className='text-light'>Fullstack Developer</h5>
                 <CTA />
+                <HeaderSocials />
+
+                {/* Profile Picture */}
+                <div className="me">
+                    <img src={ME} alt=""></img>
+                </div>
+
+                {/*Scroll Down section */}
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+                {/*Social Media Buttons */}
+
             </div>
         </header>
     )
