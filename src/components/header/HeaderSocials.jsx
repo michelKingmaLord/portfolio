a6c65f1d039fe7ce355ec63a54fd943df3151d56
Michel PC <0801342@csfoy.ca>
2022-03-15 13:12:03 -0400

    Adjustments in Header socials regarding icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BsLinkedin } from 'react-icons/bs';
-import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithubSquare } from 'react-icons/fa';
+import { FaDribbbleSquare } from 'react-icons/fa';
+
 
 
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-            <a href="https://github.com" target="_blank"><FaGithub /></a>
-            <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
+            <a href="https://linkedin.com" target="https://www.linkedin.com/in/michelkingmalord/"><FaLinkedin /></a>
+            <a href="https://github.com" target="_blank"><FaGithubSquare /></a>
+            <a href="https://dribbble.com" target="_blank"><FaDribbbleSquare /></a>
         </div>
     )
 }
