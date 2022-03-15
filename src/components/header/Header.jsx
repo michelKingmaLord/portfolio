e4316c595d89aefa6aa23cc416630cac4204ea04
Michel PC <0801342@csfoy.ca>
2022-03-15 14:14:20 -0400

    Advancement in Header CSS and ajdustments regarding img insertions and object placements

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/michelkl.jpeg';
+import ME from '../../assets/michelkl2.png';
 import HeaderSocials from './HeaderSocials';
 
 
