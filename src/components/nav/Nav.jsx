8e7f7471536f51522a500d9bf07a4b0a9418160d
Michel PC <0801342@csfoy.ca>
2022-03-15 23:28:10 -0400

    Sectionning of different components for navigation slidebar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './nav.css';
+import { ImHome } from 'react-icons/im';
+import { RiUser3Fill } from 'react-icons/ri';
+import { ImBook } from 'react-icons/im';
+import { RiServiceFill } from 'react-icons/ri';
+import { GiOpenFolder } from 'react-icons/gi';
+import { SiGooglemessages } from 'react-icons/si';
+
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <a href='#'><ImHome /></a>
+            <a href='#about'><RiUser3Fill /></a>
+            <a href='#experience'><ImBook /></a>
+            <a href='#services'><RiServiceFill /></a>
+            <a href='#portfolio'><GiOpenFolder /></a>
+            <a href='#contact'><SiGooglemessages /></a>
+        </nav>
     )
 }
 
