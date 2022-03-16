3e76c857c920bc13e8f58f0c3a443211bb573c1f
Michel PC <0801342@csfoy.ca>
2022-03-16 00:48:22 -0400

    Creating and adjustments of the navigation component (Hover buttons and sections)

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import './nav.css';
 import { ImHome } from 'react-icons/im';
 import { RiUser3Fill } from 'react-icons/ri';
@@ -9,15 +10,16 @@ import { SiGooglemessages } from 'react-icons/si';
 
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
         <nav>
-            <a href='#'><ImHome /></a>
-            <a href='#about'><RiUser3Fill /></a>
-            <a href='#experience'><ImBook /></a>
-            <a href='#services'><RiServiceFill /></a>
-            <a href='#portfolio'><GiOpenFolder /></a>
-            <a href='#contact'><SiGooglemessages /></a>
-        </nav>
+            <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><ImHome /></a>
+            <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUser3Fill /></a>
+            <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><ImBook /></a>
+            <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
+            <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><GiOpenFolder /></a>
+            <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><SiGooglemessages /></a>
+        </nav >
     )
 }
 
