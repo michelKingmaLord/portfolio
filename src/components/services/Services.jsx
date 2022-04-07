124e4dfbdf156d91558ec9e8467819dcdea033a4
Michel PC <0801342@csfoy.ca>
2022-04-06 22:53:17 -0400

    Creation of Services JSX component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,122 @@
 import React from 'react';
 import './services.css';
+import { BsCheck2Circle } from 'react-icons/bs';
 
 const Services = () => {
     return (
-        <section id='services'>Services</section>
+        <section id='services'>
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services__container">
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF UI/UX */}
+
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF Web Development */}
+
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Content Creation</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+
+                        <li>
+                            <BsCheck2Circle className='service__list-icon' />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consectetur modi ipsum.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF Content Creation */}
+            </div>
+        </section>
     )
 }
 
