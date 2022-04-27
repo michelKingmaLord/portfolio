a110173e7ff948aaa736e6f362b1997491b48bf3
Michel PC <0801342@csfoy.ca>
2022-04-26 23:58:39 -0400

    Creation of contact jsx components  for form options options

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -34,7 +34,12 @@ const Contact = () => {
                     </article>
 
                     {/* END OF CONTACT OPTIONS */}
-                    <form action=""></form>
+                    <form action="">
+                        <input type="text" name="name" placeholder="Your Full Name" required />
+                        <input type="email" name="email" placeholder="Your Email" required />
+                        <textarea name="message" rows="10" placeholder="Your Message" required></textarea>
+                        <button type='submit' className='btn btn-primary'>Send Message</button>
+                    </form>
                 </div>
             </div>
         </section>
